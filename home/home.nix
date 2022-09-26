d428d941a5aa510c6760ff18ba646af5fd0bd106
Arul Agrawal <me@arul.io>
2022-09-26 22:17:46 +0200

    move starship prompt config to nix

--- home/home.nix
+++ home/home.nix
@@ -125,6 +125,65 @@
       starship = {
         enable = true;
         enableZshIntegration = true;
+        settings = {
+          format = lib.concatStrings [
+            "$username"
+            "$hostname"
+            "$directory"
+            "$git_branch"
+            "$git_state"
+            "$git_status"
+            "$cmd_duration"
+            "$line_break"
+            "$python"
+            "$character"
+          ];
+          directory = {
+            style = "blue";
+            truncation_length = 10;
+            truncation_symbol = ".../";
+            truncate_to_repo = false;
+          };
+
+          character = {
+            success_symbol = "[❯](purple)";
+            error_symbol = "[❯](red)";
+            vicmd_symbol = "[❮](green)";
+          };
+
+          git_branch = {
+            format = "[$branch]($style)";
+            style = "bright-black";
+          };
+
+          # yes these zero width spaces are on purpose
+          git_status = {
+            format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+            style = "cyan";
+            conflicted = "​";
+            untracked = "​";
+            modified = "​";
+            staged = "​";
+            renamed = "​";
+            deleted = "​";
+            stashed = "≡";
+          };
+
+          git_state = {
+            format = "\([$state( $progress_current/$progress_total)]($style)\) ";
+            style = "bright-black";
+          };
+
+          cmd_duration = {
+            format = "[$duration]($style) ";
+            style = "yellow";
+          };
+
+          python = {
+            format = "[$virtualenv]($style) ";
+            style = "bright-black";
+          };
+        };
       };
     };
     xdg = { enable = true; };
