2657dd9eafae29162ea4a5e49d50fb52db94f905
Arul Agrawal <me@arul.io>
2022-11-01 22:57:59 +0200

    add some app store apps related to safari extensions

--- homebrew.nix
+++ homebrew.nix
@@ -31,6 +31,10 @@
       "Hidden Bar" = 1452453066;
       "Infuse" = 1136220934;
       "Intermission" = 1439431081;
+      "Adguard for Safari" = 1440147259;
+      "Bitwarden" = 1352778147;
+      "Tampermonkey" = 1482490089;
+      "Noir" = 1592917505;
     };
   };
 }
