adcedf7113cec24eae59a13af5230077a7a739ce
Arul Agrawal <me@arul.io>
2022-11-24 16:13:54 +0200

    small fix for mkcd function

--- home/shell/plugins/functions.plugin.zsh
+++ home/shell/plugins/functions.plugin.zsh
@@ -99,7 +99,7 @@ mkcd() {
     if [ ! -n "$1" ]; then
         echo "Enter a directory name"
     elif [ -d $1 ]; then
-        echo "\`$1' already exists"
+        echo "'$1' already exists"
     else
         mkdir -p "$1" && cd "$1"
     fi
