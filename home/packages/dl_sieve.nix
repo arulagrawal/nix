26170c11abbba84149fc69685e10c016e8a5de0c
Arul Agrawal <me@arul.io>
2022-11-24 16:12:55 +0200

    update dl_sieve

--- home/packages/dl_sieve.nix
+++ home/packages/dl_sieve.nix
@@ -3,15 +3,15 @@ let
   inherit (pkgs) rustPlatform fetchFromGitea;
 in rustPlatform.buildRustPackage rec {
   pname = "dl_sieve";
-  version = "0.1.5";
+  version = "0.1.7";
 
   src = fetchFromGitea {
     domain = "git.arul.io";
     owner = "arul";
     repo = pname;
     rev = version;
-    sha256 = "sha256-xRyXgx9cU4k8FxWKx2FaN1x6B8cwWnrNcJRtYYR5rm8=";
+    sha256 = "sha256-10ICnzKSrAKnQhBLRa8Fwm3pn46XiThbtu/B8YMdJ1U=";
   };
 
-  cargoSha256 = "sha256-1K8WRx/vXhFC32ycysxSS0kLyUkNBZo/z007xkfl3S8=";
+  cargoSha256 = "sha256-/hmwcdE4gIivhjRlQMDFDBFh/6eYhAKGoiOGkQMqb3g=";
 }
