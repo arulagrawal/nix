7abb3602cf34773e9a53aa0be08b88e14875513c
Arul Agrawal <me@arul.io>
2023-10-14 00:01:53 +0200

    add catppuccin colors to starship

--- nix/homeConfigurations/shell/shell.nix
+++ nix/homeConfigurations/shell/shell.nix
@@ -97,71 +97,85 @@
       enable = true;
       enableZshIntegration = true;
     };
-    starship = {
+    starship = let
+      flavour = "mocha";
+    in {
       enable = true;
       enableZshIntegration = true;
-      settings = {
-        command_timeout = 1000;
-        format = lib.concatStrings [
-          "$username"
-          "$hostname"
-          "$directory"
-          "$git_branch"
-          "$git_state"
-          "$git_status"
-          "$cmd_duration"
-          "$line_break"
-          "$python"
-          "$character"
-        ];
-        directory = {
-          style = "bold blue";
-          truncation_length = 10;
-          truncation_symbol = "";
-          truncate_to_repo = false;
-        };
+      settings =
+        {
+          command_timeout = 1000;
+          format = lib.concatStrings [
+            "$username"
+            "$hostname"
+            "$directory"
+            "$git_branch"
+            "$git_state"
+            "$git_status"
+            "$cmd_duration"
+            "$line_break"
+            "$python"
+            "$character"
+          ];
+          directory = {
+            style = "bold blue";
+            truncation_length = 10;
+            truncation_symbol = "";
+            truncate_to_repo = false;
+          };
 
-        character = {
-          success_symbol = "[❯](purple)";
-          error_symbol = "[❯](red)";
-          vicmd_symbol = "[❮](green)";
-        };
+          character = {
+            success_symbol = "[❯](pink)";
+            error_symbol = "[❯](red)";
+            vicmd_symbol = "[❮](green)";
+          };
 
-        git_branch = {
-          format = "[$branch]($style)";
-          style = "green";
-        };
+          git_branch = {
+            format = "[$branch]($style)";
+            style = "green";
+          };
 
-        # yes these zero width spaces are on purpose
-        git_status = {
-          format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
-          style = "cyan";
-          conflicted = "​";
-          untracked = "​";
-          modified = "​";
-          staged = "​";
-          renamed = "​";
-          deleted = "​";
-          stashed = "≡";
-          ahead = "↑ ";
-          behind = "↓ ";
-        };
+          # yes these zero width spaces are on purpose
+          git_status = {
+            format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+            style = "sky";
+            conflicted = "​";
+            untracked = "​";
+            modified = "​";
+            staged = "​";
+            renamed = "​";
+            deleted = "​";
+            stashed = "≡";
+            ahead = "↑ ";
+            behind = "↓ ";
+          };
 
-        git_state = {
-          format = "([$state( $progress_current/$progress_total)]($style)) ";
-          style = "bright-black";
-        };
+          git_state = {
+            format = "([$state( $progress_current/$progress_total)]($style)) ";
+            style = "surface2";
+          };
 
-        cmd_duration = {
-          format = "[$duration]($style) ";
-          style = "yellow";
-        };
+          cmd_duration = {
+            format = "[$duration]($style) ";
+            style = "yellow";
+          };
 
-        python = {
-          format = "[$virtualenv]($style) ";
-          style = "bright-black";
-        };
-      };
+          python = {
+            format = "[$virtualenv]($style) ";
+            style = "surface2";
+          };
+
+          palette = "catppuccin_${flavour}";
+        }
+        // builtins.fromTOML (builtins.readFile
+          (pkgs.fetchFromGitHub
+            {
+              owner = "catppuccin";
+              repo = "starship";
+              rev = "5629d2356f62a9f2f8efad3ff37476c19969bd4f"; # Replace with the latest commit hash
+              sha256 = "sha256-nsRuxQFKbQkyEI4TXgvAjcroVdG+heKX5Pauq/4Ota0=";
+            }
+            + /palettes/${flavour}.toml));
     };
   };
 }
