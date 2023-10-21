1925d56a49df4816e2c759378fc854ecd2524a02
Arul Agrawal <me@arul.io>
2023-10-21 15:01:12 +0200

    add remaining functions

--- nix/homeConfigurations/shell/fish/functions.nix
+++ nix/homeConfigurations/shell/fish/functions.nix
@@ -1,4 +1,14 @@
 {
+  pa = ''
+    if not test -f $argv[1]
+        echo "give a file name"
+        return 1
+    end
+
+    curl --netrc-file ~/.config/.netrc -sF "file=@$argv[1]" "https://arul.io" | pbcopy
+  '';
+  pad = "curl --netrc-file ~/.config/.netrc -X DELETE \"https://arul.io/$argv[1]\"";
+  send = "curl --netrc-file ~/.config/.netrc -d \"msg=$argv[1]\" \"https://notif-server.arul.io/msg\"";
   fzfp = ''
     fzf --reverse --inline-info --preview="if test (file --mime {} | string match -r 'binary');
             echo '{} is a binary file';
@@ -8,6 +18,27 @@
         end" --bind '?:toggle-preview' --tabstop=1 --ansi --delimiter / --with-nth -1
   '';
   dots = "find ~/nix -type f | awk '!/git|after|lua|.DS_Store/'| fzfp | xargs $EDITOR";
+  mkcd = ''
+    # Check for arguments
+    if test (count $argv) -eq 0
+        echo "Please provide a directory name."
+        return 1
+    end
+
+    # Create the directory
+    mkdir -p $argv[1]
+    if test $status -ne 0
+        echo "Error creating directory $argv[1]."
+        return 1
+    end
+
+    # Change to the directory
+    cd $argv[1]
+    if test $status -ne 0
+        echo "Error changing to directory $argv[1]."
+        return 1
+    end
+  '';
   try = ''
     set -l packages
     for i in $argv
@@ -15,4 +46,5 @@
         end
     nix shell $packages
   '';
+  clean = "nix-collect-garbage -d && sudo -i nix-collect-garbage -d";
 }
