2fda446d75714c01e30086bb1a92380d0777354e
Arul Agrawal <email@arul.io>
2024-04-25 21:48:20 +0400

    update ssh keys

--- users/config.nix
+++ users/config.nix
@@ -5,11 +5,8 @@
       name = "Arul Agrawal";
       email = "email@arul.io";
       sshKeys = [
-        # Legacy
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDgZ1J+JsIC/zEq1dIVIYAnA7f1S6KCCkhW89UU3AVuEaw8v/Mo+Lz5Djhh/3jJJS9axS3q8N5wlJw19f6HX05KwgP70bsHR8cUe/khKXzx0Xoq+qCmnI48Xx9b7wzF6Wd+tESjZQ/w7g/eAU/AEhvnk4NtUXtVYg7zhYLvELrlasKKq/k4K5L2QNQB4mTzYeZ5PZKqs9uMps+X+GXuEHVAVDqihRomQu0tWHwP48UnwZQrjqb46uUK7PgDLGmg/XNjfyws3EYsB89laU88HB9mY8cg/w5oxZx+sgYixgHHbZmo/clgEqpL/AMs3+okhNctKhg0gYmjWtTYTTWbU20QKf6adADouMYO+3rPmeYFzhUYbkMwPOY8EffoACg9ieocMyBadHYEuRSZw3ARuxl5k0305KATSWyENaz7G+hCZsgLC92MnHlcivkqxKT/HVMa1E2MVEP6SYtvdD6h0njuRVFWvUCd71ao05puPHAZXsX11+tZZfyjGDEoxfzI7yU= arul@refrigerator"
-        # New
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDBaPdFywjYm/6ombr6/E7yLTitHdtMB+g7X3D31wW2qm1lWPZK+rRI0vq+gABXH/kjV0F0xBQC80aGLpPlioyXPxuVbFRNyKw5XD0n+I7Zv3/c2b9Jp14D+WybaFvE7Rm3RB5A2cj2RjC5b72xPMsM1AADnZqxqNaUbzNx6F4somv9kXrsnsyR8uiZnN+5MssXmXp6lZxtfZ2bXYHS9y6KU+PKOGwb89Ywya1drAz8/SEFmCzvdpbGeGNAYn9s/vjCYZ+UhtwgV5Nu8Kk0NvIQHbI7RsUzVgQKnNUMFZsmPZAVyySUDPjkUf3c/eOHgorbPeG5sroMEFV2yQsXl2G2cF9qBrOI1xP1GVS3/v6/dh0dG2JEa4ye3RSUAY5ZP1iSXbLrKr/SRPxlUGvRXLcwpe4mNBLE6LVokTFkvXI3pIfcn2fD4PWca9ZLWG4/qqwuxgPlYp911lotjG+FhaYvI3AnQcLU+IYP3q8k0LytNmLVqhnNbrCDkVYLwSXXA9NY+cHySE9QfqOxwR0HlnVGloBYJAK//F5lZLudfnvaaihVlldGIc5iS3otTJDP1uCzoQkYOaIOU8H5TZU55S9acR0cgw1diHSBnR9r/a7Pt1hlYy1gRU9z7NqYJ91+lBknnAFrKlYsPBAiRRXKtvO/Hph3+/C8sQS9Vc6gzMd4Pw== me@arul.io"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH+b/vi/4h4bXgcspPoDdWN5cBya7NuWa1cHzMNmnqf+ email@arul.io"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINYdQ5b3KP0ejFGajR9m+9gXAC5rhTeJFsu0gTdCqivu email@arul.io"
       ];
     };
   };
