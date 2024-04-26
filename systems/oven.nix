a4575a8aad395d19321e623d02348c8f1f1d09ed
Arul Agrawal <email@arul.io>
2024-04-26 17:37:17 +0400

    add HA

--- systems/oven.nix
+++ systems/oven.nix
@@ -11,6 +11,7 @@ in
     "${self}/nixos/disko/trivial.nix"
     "${self}/nixos/nix.nix"
     "${self}/nixos/self/primary-as-admin.nix"
+    "${self}/nixos/server/harden.nix"
     "${self}/nixos/docker.nix"
     "${self}/nixos/virtualisation.nix"
     "${self}/nixos/tailscale.nix"
