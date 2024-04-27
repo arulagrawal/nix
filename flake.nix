bc1d3a1059fb84df79fac4d1b74411cda477641f
Arul Agrawal <email@arul.io>
2024-04-27 09:40:20 +0400

    remove duplicate inputs for nixvim

--- flake.nix
+++ flake.nix
@@ -26,6 +26,8 @@
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-parts.follows = "flake-parts";
+      inputs.home-manager.follows = "home-manager";
+      inputs.nix-darwin.follows = "nix-darwin";
     };
 
     spicetify-nix = {
