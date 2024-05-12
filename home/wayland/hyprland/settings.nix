cf7004f59e0b79f3bb3012e48ae32b03002fb5f9
Arul Agrawal <email@arul.io>
2024-05-13 03:54:44 +0400

    add monitor config to hyprland

--- home/wayland/hyprland/settings.nix
+++ home/wayland/hyprland/settings.nix
@@ -11,6 +11,8 @@ in
       "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
     ];
 
+    monitor = "DP-1,2560x1440@240,0x0,1,bitdepth,10,vrr,1";
+
     exec-once = [
       # set cursor for HL itself
       "firefox"
