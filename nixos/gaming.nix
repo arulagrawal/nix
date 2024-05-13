09b853afe0e886f1cc2007bfaefac03fbce1015c
Arul Agrawal <email@arul.io>
2024-05-14 01:35:50 +0400

    get gamescope

--- nixos/gaming.nix
+++ nixos/gaming.nix
@@ -4,7 +4,11 @@ let
   inherit (flake) inputs;
 in
 {
-  programs.steam.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    gamescopeSession.enable = true;
+  };
   environment.systemPackages = with pkgs; [
     inputs.nix-gaming.packages.${pkgs.system}.wine-ge
   ];
