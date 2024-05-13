4b2937228c0861a19e4d270bf575696674f253c6
Arul Agrawal <email@arul.io>
2024-05-14 01:35:57 +0400

    play with font settings

--- nixos/gui/fonts.nix
+++ nixos/gui/fonts.nix
@@ -2,15 +2,31 @@
 
 {
   fonts = {
-    enableDefaultPackages = true;
+    enableDefaultPackages = false;
 
     packages = with pkgs; [
       # NOTE: Some fonts may break colour emojis in Chrome
       # cf. https://github.com/NixOS/nixpkgs/issues/69073#issuecomment-621982371
       # If this happens , keep noto-fonts-emoji and try disabling others (nerdfonts, etc.)
+      roboto
+      noto-fonts
       noto-fonts-emoji
       noto-fonts-cjk
       fira-code-nerdfont
     ];
+
+    fontconfig.subpixel = {
+      rgba = "rgb";
+      lcdfilter = "default";
+    };
+
+    fontconfig.hinting.style = "slight";
+
+    fontconfig.defaultFonts = {
+      serif = [ "Noto Serif" "Noto Color Emoji" ];
+      sansSerif = [ "Noto Sans" "Noto Color Emoji" ];
+      monospace = [ "FiraCode Nerd Font Mono" "Noto Color Emoji" ];
+      emoji = [ "Noto Color Emoji" ];
+    };
   };
 }
