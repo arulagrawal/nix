976c2ab1e41ac908c54000128f40efff5ac781ab
Arul Agrawal <email@arul.io>
2024-05-14 03:48:50 +0400

    fix steam gamescope

--- nixos/gaming.nix
+++ nixos/gaming.nix
@@ -9,7 +9,26 @@ in
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     gamescopeSession.enable = true;
   };
-  environment.systemPackages = with pkgs; [
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        libgdiplus
+        keyutils
+        libkrb5
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXScrnSaver
+      ];
+    };
+  };
+
+  environment.systemPackages = [
     inputs.nix-gaming.packages.${pkgs.system}.wine-ge
   ];
 }
