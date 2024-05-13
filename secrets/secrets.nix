9b26a01d3225db71e71af6efadf9f3aaed940a94
Arul Agrawal <email@arul.io>
2024-05-13 08:45:36 +0400

    add new Ed25519 ssh key for laptop

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,9 @@
 let
-  arul = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINYdQ5b3KP0ejFGajR9m+9gXAC5rhTeJFsu0gTdCqivu email@arul.io";
-  users = [ arul ];
+  arul_pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINYdQ5b3KP0ejFGajR9m+9gXAC5rhTeJFsu0gTdCqivu email@arul.io";
+  arul_laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP3pbkO1VUUJha4fPyYTxHrTGzwNz1rPl0QWMpPp9Y01 email@arul.io";
+  users = [ arul_pc arul_laptop ];
 
+  # these are host keys
   refrigerator = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBPbO8B3dMVCdP1hP27X9s5FBJXGTFTKTx9Vxc5Husmd";
   kettle = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINHmGt+HJZ5tyPa6aYHNV6aqOx5fyYAQ5i91W1fvSIbB";
   oven = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP+4JtVo9WcmqR02yhLx4fPYhwuZDLMon3xR4Vsa/Rpk";
