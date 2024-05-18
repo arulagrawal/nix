d9ac31ae9f9d174f6f8378c2b70004546530ab2d
Arul Agrawal <email@arul.io>
2024-05-18 19:13:19 +0400

    no longer need tailscale overlay...

--- packages/overlay.nix
+++ packages/overlay.nix
@@ -13,27 +13,27 @@ self: super: {
   notif = inputs.notif.packages.${system}.default;
   agenix = inputs.agenix.packages.${system}.default;
 
-  tailscale =
-    let
-      version = "1.66.1";
-      src = super.fetchFromGitHub {
-        owner = "tailscale";
-        repo = "tailscale";
-        rev = "v${version}";
-        hash = "sha256-1Yt8W/UanAghaElGiD+z7BKeV/Ge+OElA+B9yBnu3vw=";
-      };
-    in
-    (super.tailscale.override {
-      buildGoModule = args: super.buildGoModule (args // {
-        inherit src version;
-        vendorHash = "sha256-Hd77xy8stw0Y6sfk3/ItqRIbM/349M/4uf0iNy1xJGw=";
-
-        ldflags = [
-          "-w"
-          "-s"
-          "-X tailscale.com/version.longStamp=${version}"
-          "-X tailscale.com/version.shortStamp=${version}"
-        ];
-      });
-    });
+  # tailscale =
+  #   let
+  #     version = "1.66.1";
+  #     src = super.fetchFromGitHub {
+  #       owner = "tailscale";
+  #       repo = "tailscale";
+  #       rev = "v${version}";
+  #       hash = "sha256-1Yt8W/UanAghaElGiD+z7BKeV/Ge+OElA+B9yBnu3vw=";
+  #     };
+  #   in
+  #   (super.tailscale.override {
+  #     buildGoModule = args: super.buildGoModule (args // {
+  #       inherit src version;
+  #       vendorHash = "sha256-Hd77xy8stw0Y6sfk3/ItqRIbM/349M/4uf0iNy1xJGw=";
+  #
+  #       ldflags = [
+  #         "-w"
+  #         "-s"
+  #         "-X tailscale.com/version.longStamp=${version}"
+  #         "-X tailscale.com/version.shortStamp=${version}"
+  #       ];
+  #     });
+  #   });
 }
