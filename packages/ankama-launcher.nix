8e15944779701ec8abfc54368fcbf4ab0353e6d5
Arul Agrawal <email@arul.io>
2024-08-31 13:54:27 +0200

    update ankama launcher

--- packages/ankama-launcher.nix
+++ packages/ankama-launcher.nix
@@ -4,7 +4,7 @@ let
   src = pkgs.fetchurl {
     url = "https://launcher.cdn.ankama.com/installers/production/Ankama%20Launcher-Setup-x86_64.AppImage";
     # nix store prefetch-file "https://launcher.cdn.ankama.com/installers/production/Ankama%20Launcher-Setup-x86_64.AppImage" --name ankama-launcher.AppImage
-    hash = "sha256-aTzTQRwIpP87rn/GJ958y9nKbkuZ1wai2a4oNBLJ7bU=";
+    hash = "sha256-aD5YYcye/WPpb0Kir55qCL+RGbK8ZSdmi4NKmqKkW/Y=";
     name = "ankama-launcher.AppImage";
   };
 
