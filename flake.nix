1921da9e2db60c7906df33fa1f12c2a4c4e1715f
Arul Agrawal <email@arul.io>
2025-01-07 12:04:45 +0100

    go back to upstream nix-darwin

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     home-manager.url = "github:arulagrawal/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    nix-darwin.url = "github:arulagrawal/nix-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     nixos-unified.url = "github:srid/nixos-unified";
